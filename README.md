354b369368f479dbdbd391d94aa1be343ea5bba5
Satya Sandeep <satyasandeep786@gmail.com>
2020-05-31 09:11:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# casing-converter
+
+> Transform strings and numbers to INR format.
+
+## Installation
+
+```sh
+$ npm install inr-converter
+```
+
+## Usage
+
+```javascript
+'use strict';
+const { toRupees } = require("inr-converter");
+
+
+console.log(toRupees("200000"));
+console.log(toRupees(20000000));
+
+
+/** 
+ * Output :
+ * 
+ * 2,00,000
+ * 2,00,00,000
+ * 
+*/
+
+```
+
+## License
+
+ISC
---
+++ index.js
@@ -0,0 +1,15 @@
+const toRupees = (str) => {
+    str = str.toString();
+    let lastThree = str.substring(str.length - 3);
+    let otherNumbers = str.substring(0, str.length - 3);
+    if (otherNumbers !== '')
+        lastThree = ',' + lastThree;
+    return otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+
+};
+
+console.log(toRupees("20000000"));
+
+
+
+module.exports = { toRupees };
